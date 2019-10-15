346c8e65ce82d8c1cec0da880a7114979ab935f9
Santiago Cardona <scardona@lean-tech.io>
2019-10-15 09:47:38 -0500

    First commit

---
+++ utils/mocks/movies.js
@@ -0,0 +1,140 @@
+const moviesMock = [{
+  "id": "8e795fed-e82b-40d6-bf48-fcd057c822f2",
+  "title": "Cannibal Women in the Avocado Jungle of Death",
+  "year": 1992,
+  "cover": "http://dummyimage.com/221x236.jpg/dddddd/000000",
+  "description": "Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.\n\nIn hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.\n\nAliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.",
+  "duration": 54,
+  "contentRating": null,
+  "source": "https://nifty.com/sodales/scelerisque/mauris.js?praesent=est&id=risus&massa=auctor&id=sed&nisl=tristique&venenatis=in&lacinia=tempus&aenean=sit&sit=amet&amet=sem&justo=fusce&morbi=consequat&ut=nulla&odio=nisl&cras=nunc&mi=nisl&pede=duis&malesuada=bibendum",
+  "tags": [
+    "Romance|Western",
+    "Comedy|Fantasy|Horror"
+  ]
+}, {
+  "id": "44c945d7-f7ee-4b17-8a67-b231426307b5",
+  "title": "Roman Polanski: Wanted and Desired",
+  "year": 2004,
+  "cover": "http://dummyimage.com/245x220.png/ff4444/ffffff",
+  "description": "Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.\n\nSed ante. Vivamus tortor. Duis mattis egestas metus.",
+  "duration": 66,
+  "contentRating": null,
+  "source": "http://auda.org.au/sit.jpg?odio=massa&cras=id&mi=lobortis&pede=convallis&malesuada=tortor&in=risus&imperdiet=dapibus&et=augue&commodo=vel&vulputate=accumsan&justo=tellus&in=nisi&blandit=eu&ultrices=orci&enim=mauris&lorem=lacinia&ipsum=sapien&dolor=quis&sit=libero&amet=nullam&consectetuer=sit&adipiscing=amet&elit=turpis&proin=elementum&interdum=ligula&mauris=vehicula&non=consequat&ligula=morbi&pellentesque=a&ultrices=ipsum&phasellus=integer&id=a&sapien=nibh&in=in&sapien=quis&iaculis=justo&congue=maecenas",
+  "tags": [
+    "Drama",
+    "Drama",
+    "Comedy"
+  ]
+}, {
+  "id": "bdbe12f7-2499-4574-812a-2e56ae1266db",
+  "title": "Cave, The",
+  "year": 2001,
+  "cover": "http://dummyimage.com/165x106.jpg/5fa2dd/ffffff",
+  "description": "Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.\n\nPraesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.\n\nCras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+  "duration": 60,
+  "contentRating": null,
+  "source": "https://w3.org/vulputate.xml?vitae=pellentesque&ipsum=volutpat&aliquam=dui&non=maecenas&mauris=tristique&morbi=est&non=et&lectus=tempus&aliquam=semper&sit=est&amet=quam&diam=pharetra&in=magna&magna=ac&bibendum=consequat&imperdiet=metus&nullam=sapien&orci=ut&pede=nunc&venenatis=vestibulum&non=ante&sodales=ipsum&sed=primis&tincidunt=in&eu=faucibus&felis=orci&fusce=luctus&posuere=et&felis=ultrices&sed=posuere&lacus=cubilia&morbi=curae&sem=mauris&mauris=viverra&laoreet=diam&ut=vitae&rhoncus=quam&aliquet=suspendisse&pulvinar=potenti&sed=nullam&nisl=porttitor&nunc=lacus&rhoncus=at&dui=turpis&vel=donec&sem=posuere&sed=metus&sagittis=vitae&nam=ipsum&congue=aliquam&risus=non&semper=mauris&porta=morbi&volutpat=non&quam=lectus&pede=aliquam&lobortis=sit&ligula=amet&sit=diam&amet=in&eleifend=magna&pede=bibendum&libero=imperdiet&quis=nullam&orci=orci&nullam=pede&molestie=venenatis&nibh=non&in=sodales&lectus=sed&pellentesque=tincidunt&at=eu&nulla=felis&suspendisse=fusce&potenti=posuere&cras=felis&in=sed",
+  "tags": [
+    "Crime|Horror|Thriller"
+  ]
+}, {
+  "id": "d68e10c8-26ae-4c66-8f7e-ae31b7a5d464",
+  "title": "Donnie Darko",
+  "year": 1992,
+  "cover": "http://dummyimage.com/108x195.png/cc0000/ffffff",
+  "description": "Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.\n\nAenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.",
+  "duration": 26,
+  "contentRating": null,
+  "source": "http://google.ru/at.json?eget=a&vulputate=feugiat&ut=et&ultrices=eros&vel=vestibulum&augue=ac&vestibulum=est&ante=lacinia&ipsum=nisi&primis=venenatis&in=tristique&faucibus=fusce&orci=congue&luctus=diam&et=id&ultrices=ornare&posuere=imperdiet&cubilia=sapien&curae=urna&donec=pretium&pharetra=nisl&magna=ut&vestibulum=volutpat&aliquet=sapien&ultrices=arcu&erat=sed&tortor=augue&sollicitudin=aliquam&mi=erat&sit=volutpat&amet=in&lobortis=congue&sapien=etiam&sapien=justo&non=etiam&mi=pretium&integer=iaculis&ac=justo&neque=in&duis=hac&bibendum=habitasse&morbi=platea&non=dictumst&quam=etiam&nec=faucibus&dui=cursus&luctus=urna&rutrum=ut&nulla=tellus&tellus=nulla&in=ut&sagittis=erat&dui=id&vel=mauris&nisl=vulputate&duis=elementum&ac=nullam&nibh=varius&fusce=nulla&lacus=facilisi&purus=cras&aliquet=non&at=velit&feugiat=nec&non=nisi&pretium=vulputate&quis=nonummy&lectus=maecenas",
+  "tags": [
+    "Action|Comedy|Crime",
+    "Drama",
+    "Children|Comedy|Musical"
+  ]
+}, {
+  "id": "959a6a35-98d3-4a15-aba8-a04b21609e6d",
+  "title": "Gun Shy",
+  "year": 2008,
+  "cover": "http://dummyimage.com/111x154.png/5fa2dd/ffffff",
+  "description": "Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.\n\nMaecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.\n\nCurabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.",
+  "duration": 40,
+  "contentRating": null,
+  "source": "https://pinterest.com/amet/erat/nulla.js?nam=vulputate&nulla=luctus&integer=cum&pede=sociis&justo=natoque&lacinia=penatibus&eget=et&tincidunt=magnis&eget=dis&tempus=parturient&vel=montes&pede=nascetur&morbi=ridiculus&porttitor=mus&lorem=vivamus&id=vestibulum&ligula=sagittis",
+  "tags": [
+    "Drama",
+    "Comedy",
+    "Drama|Fantasy|Romance|War",
+    "Comedy"
+  ]
+}, {
+  "id": "3f11c1db-f8db-4970-94f6-b4da6f46d8a4",
+  "title": "Giant Gila Monster, The",
+  "year": 1992,
+  "cover": "http://dummyimage.com/157x238.bmp/dddddd/000000",
+  "description": "Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.\n\nMaecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.",
+  "duration": 76,
+  "contentRating": null,
+  "source": "http://blog.com/faucibus.xml?nibh=tellus&in=in&hac=sagittis&habitasse=dui&platea=vel&dictumst=nisl&aliquam=duis&augue=ac&quam=nibh&sollicitudin=fusce&vitae=lacus&consectetuer=purus&eget=aliquet&rutrum=at&at=feugiat&lorem=non&integer=pretium&tincidunt=quis&ante=lectus&vel=suspendisse&ipsum=potenti&praesent=in&blandit=eleifend&lacinia=quam&erat=a&vestibulum=odio&sed=in&magna=hac&at=habitasse&nunc=platea&commodo=dictumst&placerat=maecenas&praesent=ut&blandit=massa&nam=quis&nulla=augue&integer=luctus&pede=tincidunt&justo=nulla&lacinia=mollis&eget=molestie&tincidunt=lorem&eget=quisque&tempus=ut&vel=erat&pede=curabitur&morbi=gravida&porttitor=nisi&lorem=at&id=nibh&ligula=in&suspendisse=hac&ornare=habitasse&consequat=platea&lectus=dictumst&in=aliquam&est=augue&risus=quam&auctor=sollicitudin&sed=vitae&tristique=consectetuer&in=eget&tempus=rutrum&sit=at&amet=lorem&sem=integer&fusce=tincidunt&consequat=ante&nulla=vel&nisl=ipsum&nunc=praesent&nisl=blandit&duis=lacinia&bibendum=erat&felis=vestibulum&sed=sed&interdum=magna&venenatis=at&turpis=nunc&enim=commodo&blandit=placerat&mi=praesent&in=blandit&porttitor=nam&pede=nulla&justo=integer&eu=pede&massa=justo&donec=lacinia&dapibus=eget&duis=tincidunt&at=eget&velit=tempus&eu=vel&est=pede&congue=morbi",
+  "tags": [
+    "Comedy"
+  ]
+}, {
+  "id": "15e94750-03a2-448a-a401-8070286aa6b0",
+  "title": "Hornets' Nest",
+  "year": 2004,
+  "cover": "http://dummyimage.com/221x188.bmp/ff4444/ffffff",
+  "description": "Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.\n\nMorbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.",
+  "duration": 83,
+  "contentRating": null,
+  "source": "https://bizjournals.com/suspendisse.xml?maecenas=lacinia&tristique=sapien&est=quis&et=libero&tempus=nullam&semper=sit&est=amet&quam=turpis&pharetra=elementum&magna=ligula&ac=vehicula&consequat=consequat&metus=morbi&sapien=a&ut=ipsum&nunc=integer&vestibulum=a&ante=nibh&ipsum=in&primis=quis&in=justo&faucibus=maecenas&orci=rhoncus&luctus=aliquam&et=lacus&ultrices=morbi&posuere=quis&cubilia=tortor&curae=id&mauris=nulla&viverra=ultrices&diam=aliquet",
+  "tags": [
+    "Comedy",
+    "Drama|Romance",
+    "Drama|Horror",
+    "Documentary"
+  ]
+}, {
+  "id": "75649d6e-2ffd-40b4-b615-6b021f4f1782",
+  "title": "Gentlemen",
+  "year": 2007,
+  "cover": "http://dummyimage.com/119x105.png/5fa2dd/ffffff",
+  "description": "Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.\n\nCum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.\n\nEtiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.",
+  "duration": 54,
+  "contentRating": null,
+  "source": "http://nature.com/augue/quam/sollicitudin.aspx?nec=velit&euismod=vivamus&scelerisque=vel&quam=nulla&turpis=eget&adipiscing=eros&lorem=elementum&vitae=pellentesque&mattis=quisque&nibh=porta&ligula=volutpat&nec=erat&sem=quisque&duis=erat&aliquam=eros&convallis=viverra&nunc=eget&proin=congue&at=eget&turpis=semper&a=rutrum&pede=nulla&posuere=nunc&nonummy=purus&integer=phasellus&non=in&velit=felis&donec=donec&diam=semper&neque=sapien&vestibulum=a&eget=libero&vulputate=nam&ut=dui&ultrices=proin&vel=leo&augue=odio&vestibulum=porttitor&ante=id&ipsum=consequat&primis=in&in=consequat&faucibus=ut&orci=nulla&luctus=sed&et=accumsan&ultrices=felis&posuere=ut&cubilia=at&curae=dolor&donec=quis&pharetra=odio&magna=consequat&vestibulum=varius&aliquet=integer&ultrices=ac&erat=leo&tortor=pellentesque&sollicitudin=ultrices&mi=mattis&sit=odio&amet=donec&lobortis=vitae&sapien=nisi&sapien=nam&non=ultrices&mi=libero&integer=non&ac=mattis&neque=pulvinar&duis=nulla&bibendum=pede&morbi=ullamcorper&non=augue&quam=a&nec=suscipit&dui=nulla&luctus=elit&rutrum=ac&nulla=nulla&tellus=sed&in=vel&sagittis=enim",
+  "tags": [
+    "Comedy|Drama",
+    "Drama"
+  ]
+}, {
+  "id": "ab5998a1-e03e-4980-988a-af9a783f864a",
+  "title": "Blue Bird, The",
+  "year": 1998,
+  "cover": "http://dummyimage.com/159x102.jpg/ff4444/ffffff",
+  "description": "Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.\n\nAenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.",
+  "duration": 28,
+  "contentRating": null,
+  "source": "http://cdbaby.com/turpis/integer/aliquet.js?donec=urna&quis=pretium&orci=nisl&eget=ut&orci=volutpat&vehicula=sapien&condimentum=arcu&curabitur=sed&in=augue",
+  "tags": [
+    "Comedy",
+    "Drama",
+    "Horror|Thriller"
+  ]
+}, {
+  "id": "2a12a363-6cd4-4e84-8da1-1f39193e4564",
+  "title": "Nameless, The (Los sin nombre)",
+  "year": 2009,
+  "cover": "http://dummyimage.com/211x187.jpg/5fa2dd/ffffff",
+  "description": "Sed ante. Vivamus tortor. Duis mattis egestas metus.\n\nAenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.\n\nQuisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros.",
+  "duration": 17,
+  "contentRating": null,
+  "source": "https://homestead.com/quis/tortor/id/nulla.aspx?nunc=nulla&commodo=justo&placerat=aliquam&praesent=quis&blandit=turpis&nam=eget&nulla=elit&integer=sodales&pede=scelerisque&justo=mauris&lacinia=sit&eget=amet&tincidunt=eros&eget=suspendisse&tempus=accumsan&vel=tortor&pede=quis&morbi=turpis&porttitor=sed&lorem=ante&id=vivamus&ligula=tortor&suspendisse=duis&ornare=mattis&consequat=egestas&lectus=metus&in=aenean&est=fermentum&risus=donec&auctor=ut&sed=mauris&tristique=eget&in=massa&tempus=tempor&sit=convallis&amet=nulla&sem=neque&fusce=libero&consequat=convallis&nulla=eget&nisl=eleifend&nunc=luctus&nisl=ultricies&duis=eu&bibendum=nibh&felis=quisque&sed=id&interdum=justo&venenatis=sit&turpis=amet&enim=sapien&blandit=dignissim&mi=vestibulum&in=vestibulum&porttitor=ante&pede=ipsum&justo=primis&eu=in&massa=faucibus&donec=orci&dapibus=luctus&duis=et&at=ultrices&velit=posuere&eu=cubilia&est=curae&congue=nulla&elementum=dapibus&in=dolor&hac=vel&habitasse=est&platea=donec&dictumst=odio&morbi=justo&vestibulum=sollicitudin&velit=ut",
+  "tags": [
+    "Drama|War",
+    "Documentary"
+  ]
+}]
+
+module.exports = {
+  moviesMock
+}
